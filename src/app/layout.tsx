e41bab936dde3b548eace6dd8b7d5e9de91d90ed
arnavnagpurkar <arnavn.dev@gmail.com>
2024-04-11 13:17:38 +0530

    Google Fonts Poppins

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,6 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
 import Head from "next/head";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "ERP clone better than the orignal",
@@ -18,9 +15,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <Head>
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"/>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" />
       </Head>
-      <body className={inter.className}>{children}</body>
+      <body >
+        {children}
+      </body>
     </html>
   );
 }
