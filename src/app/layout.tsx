b46538c3c326bef17056bc35e6e23f7ab9ed477b
pranav731 <pranavsbawkar@gmail.com>
2024-04-11 13:09:52 +0530

    Login screen

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,6 +17,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"/>
+      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   );
