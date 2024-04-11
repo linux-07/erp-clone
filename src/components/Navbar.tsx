e49c956195d07340d7f1868d8934ddb44b9154a4
arnavnagpurkar <arnavn.dev@gmail.com>
2024-04-11 19:12:21 +0530

    Navbar bugs fixed

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,27 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 function Navbar() {
   return (
-    <nav>
-      <div className="nav-bar">
-        <div className="logo">
-          <a href="https://www.edupluscampus.com/" target="_blank">
-            <div className="educampus"></div>
-          </a>
-        </div>
+    <nav className='min-w-screen nav'>
+      <div className="nav-bar sm:px-36 px-5 flex justify-between">
+        <Image 
+          src={"/spbbs-logo.png"}
+          alt="logo"
+          width={100}
+          height={100}
+        />
+        <Link
+          href="https://www.edupluscampus.com/"
+        >
+          <Image 
+            src={"/edulogo.png"}
+            alt="logo"
+            width={100}
+            height={100}
+          />
+        </Link>
       </div>
     </nav>
   )
